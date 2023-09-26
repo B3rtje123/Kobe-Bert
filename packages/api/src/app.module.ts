3385bb164b1d58f1e68053aaa2cfec778a56c0d0
B3rtje123 <114138835+B3rtje123@users.noreply.github.com>
2023-09-26 16:29:48 +0200

    staff module deleted

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,16 +1,18 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { GraphQLModule } from '@nestjs/graphql';
+import { Module } from "@nestjs/common"
+import { AppController } from "./app.controller"
+import { AppService } from "./app.service"
+import { GraphQLModule } from "@nestjs/graphql"
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo"
+import { ClientModule } from "./client/client.module"
 import { StaffModule } from './staff/staff.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: true
+      autoSchemaFile: true,
     }),
+    ClientModule,
     StaffModule,
   ],
   controllers: [AppController],
