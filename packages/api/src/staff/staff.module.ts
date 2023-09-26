3253264cc7c60501968a5e0d5e1ae83a3c46f113
B3rtje123 <114138835+B3rtje123@users.noreply.github.com>
2023-09-26 16:44:50 +0200

    Temp Deletion

--- packages/api/src/staff/staff.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { StaffService } from './staff.service';
-import { StaffResolver } from './staff.resolver';
-
-@Module({
-  providers: [StaffResolver, StaffService],
-})
-export class StaffModule {}
