2fef0ecc84c5f80dfeb6b262b84b16f11435b07b
B3rtje123 <114138835+B3rtje123@users.noreply.github.com>
2023-09-26 16:19:39 +0200

    feat: staff entity made

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import { StaffModule } from './staff/staff.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true
+    }),
+    StaffModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
