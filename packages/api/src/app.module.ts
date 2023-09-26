3c45416fa46fd4f3b0ed20298336a355b142dd76
despeghelkobe <55882027+despeghelkobe@users.noreply.github.com>
2023-09-26 16:55:14 +0200

    feat:setup mongo & typeorm

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -4,9 +4,18 @@ import { AppService } from "./app.service"
 import { GraphQLModule } from "@nestjs/graphql"
 import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo"
 import { ClientModule } from "./client/client.module"
+import { TypeOrmModule } from "@nestjs/typeorm"
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: "mongodb",
+      url: "mongodb://localhost:27028/api",
+      entities: [__dirname + "/**/*.entity.{js,ts}"],
+      synchronize: true, // Careful with this in production
+      useNewUrlParser: true,
+      useUnifiedTopology: true, // Disable deprecated warnings
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
