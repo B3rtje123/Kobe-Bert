9cdaaf16cf8a2894c9cba41002dc731d749773a6
B3rtje123 <114138835+B3rtje123@users.noreply.github.com>
2023-09-26 17:10:16 +0200

    feat: Staff Files remade

---
+++ packages/api/src/staff/entities/staff.entity.ts
@@ -0,0 +1,10 @@
+import { ObjectType, Field, Int, ID } from "@nestjs/graphql";
+
+@ObjectType()
+export class Staff{
+    @Field(() => ID)
+    id: string;
+
+    @Field()
+    name: string;
+}
---
+++ packages/api/src/staff/staff.module.ts
@@ -0,0 +1,8 @@
+import { Module } from "@nestjs/common";
+import { StaffService } from './staff.service';
+import { StaffResolver } from './staff.resolver';
+
+@Module({
+    providers: [StaffResolver, StaffService]
+})
+export class StafModule {}
