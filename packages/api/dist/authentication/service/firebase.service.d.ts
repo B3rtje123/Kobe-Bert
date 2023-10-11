448b8b6514b334f21b7a21a23929ab4ebebebd1c
despeghelkobe <55882027+despeghelkobe@users.noreply.github.com>
2023-10-11 16:36:02 +0200

    fix:remove dist from remote

--- packages/api/dist/authentication/service/firebase.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { Auth } from "firebase-admin/lib/auth/auth";
-export declare class FirebaseService {
-    private firebaseApp;
-    constructor();
-    getAuth: () => Auth;
-}
--- packages/api/dist/authentication/service/guards/firebase.guard.d.ts
+++
@@ -1,6 +0,0 @@
-import { ExecutionContext } from "@nestjs/common";
-declare const FireBaseGuard_base: import("@nestjs/passport").Type<import("@nestjs/passport").IAuthGuard>;
-export declare class FireBaseGuard extends FireBaseGuard_base {
-    getRequest(context: ExecutionContext): any;
-}
-export {};
