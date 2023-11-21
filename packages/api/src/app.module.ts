44f0363dfd3753d3257f5137a953004b45b954a2
despeghelkobe <55882027+despeghelkobe@users.noreply.github.com>
2023-11-21 16:58:35 +0100

    feat: docker production

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -6,10 +6,10 @@ import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo"
 import { TypeOrmModule } from "@nestjs/typeorm"
 import { AuthenticationModule } from "./authentication/authentication.module"
 import { ConfigModule } from "@nestjs/config"
-import { SeedModule } from './seed/seed.module';
-import { UsersModule } from './users/users.module';
-import { TicketsModule } from './tickets/tickets.module';
-import { LocationsModule } from './locations/locations.module';
+import { SeedModule } from "./seed/seed.module"
+import { UsersModule } from "./users/users.module"
+import { TicketsModule } from "./tickets/tickets.module"
+import { LocationsModule } from "./locations/locations.module"
 
 @Module({
   imports: [
@@ -17,15 +17,18 @@ import { LocationsModule } from './locations/locations.module';
 
     TypeOrmModule.forRoot({
       type: "mongodb",
-      url: "mongodb://localhost:27028/api",
+      // url: "mongodb://localhost:27028/api",
+      url: `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, //DOCKER
       entities: [__dirname + "/**/*.entity.{js,ts}"],
-      synchronize: true, // Careful with this in production
+      // synchronize: true, // Careful with this in production
+      synchronize: process.env.NODE_ENV == "production" ? false : true,
       useNewUrlParser: true,
       useUnifiedTopology: true, // Disable deprecated warnings
     }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
+      playground: process.env.NODE_ENV == "production" ? false : true,
     }),
     AuthenticationModule,
     SeedModule,
--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from "./app.module"
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      process.env.FRONTEND_URL,
+    ],
     credentials: true,
   })
   await app.listen(3000)
