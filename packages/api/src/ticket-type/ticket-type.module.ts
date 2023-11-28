2ba7dce496e223fe0417c32de36ca7148339c661
despeghelkobe <55882027+despeghelkobe@users.noreply.github.com>
2023-11-28 16:36:27 +0100

    fix: module name

---
+++ packages/api/src/ticket-type/ticket-type.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { TicketTypeService } from './ticket-type.service';
+import { TicketTypeResolver } from './ticket-type.resolver';
+
+@Module({
+  providers: [TicketTypeResolver, TicketTypeService],
+})
+export class TicketTypeModule {}
