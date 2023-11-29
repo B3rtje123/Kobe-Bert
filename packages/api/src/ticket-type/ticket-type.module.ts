2057d381d4e500c12949721c2e075021125abf03
despeghelkobe <55882027+despeghelkobe@users.noreply.github.com>
2023-11-29 15:07:27 +0100

    fix: It doesn't work otherwise

--- packages/api/src/ticket-type/ticket-type.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TicketTypeService } from './ticket-type.service';
-import { TicketTypeResolver } from './ticket-type.resolver';
-
-@Module({
-  providers: [TicketTypeResolver, TicketTypeService],
-})
-export class TicketTypeModule {}
