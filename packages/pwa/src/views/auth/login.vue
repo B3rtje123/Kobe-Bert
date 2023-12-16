c460044ce4163425b1b876cbb91ad30a45d8c101
despeghelkobe <55882027+despeghelkobe@users.noreply.github.com>
2023-12-16 18:31:10 +0100

    feat: show tickets from user

--- packages/pwa/src/views/auth/login.vue
+++ packages/pwa/src/views/auth/login.vue
@@ -1,129 +1,142 @@
 <template>
-  <form @submit.prevent="handleLogin"
-  class="flex mt-16 justify-center items-center text-BgBlack">
-    <div class="rounded-lg max-w-sm shadow-[0_0_60px_-25px_rgba(0,0,0,0.3)] shadow-AccentBlue p-12 bg-MainWhite">
-
+  <form
+    @submit.prevent="handleLogin"
+    class="flex mt-16 justify-center items-center text-BgBlack"
+  >
+    <div
+      class="rounded-lg max-w-sm shadow-[0_0_60px_-25px_rgba(0,0,0,0.3)] shadow-AccentBlue p-12 bg-MainWhite"
+    >
       <div class="text-center m-8">
         <h1 class="text-3xl font-semibold">Log in</h1>
         <h2 class="text-xs mt-1">Enter your account details below</h2>
       </div>
 
-      <p class="text-red-500 font-medium text-center">{{ tooMany ? 'You have tried too many times.\nTry again later!' : '' }}</p>
-      <p class="text-red-500 font-medium text-center">{{ wrongLogin ? 'Email or password is wrong!' : '' }}</p>
+      <p class="text-red-500 font-medium text-center">
+        {{ tooMany ? "You have tried too many times.\nTry again later!" : "" }}
+      </p>
+      <p class="text-red-500 font-medium text-center">
+        {{ wrongLogin ? "Email or password is wrong!" : "" }}
+      </p>
 
       <div class="mb-6">
         <label for="email" class="mb-4 font-medium">Email</label>
-        <input 
-            type="text"
-            name="email"
-            id="email"
-            placeholder="example@domain.com"
-            v-model="loginCredentials.email"
-            required
-            autocomplete="off"
-            class="block rounded-lg border-2 p-2 shadow-inner shadow-gray-400 w-full transition-colors ease-in-out duration-300
-            hover:border-AccentBlue 
-            focus:outline-none focus:ring-4 focus:ring-AccentBlue"   
-            @input=""
+        <input
+          type="text"
+          name="email"
+          id="email"
+          placeholder="example@domain.com"
+          v-model="loginCredentials.email"
+          required
+          autocomplete="off"
+          class="block rounded-lg border-2 p-2 shadow-inner shadow-gray-400 w-full transition-colors ease-in-out duration-300 hover:border-AccentBlue focus:outline-none focus:ring-4 focus:ring-AccentBlue"
+          @input=""
         />
-        <p v-show="showError" class="text-red-500">{{ isValidEmail(loginCredentials.email) ? "" : "Ongeldig e-mail adres" }}</p>
+        <p v-show="showError" class="text-red-500">
+          {{
+            isValidEmail(loginCredentials.email) ? "" : "Ongeldig e-mail adres"
+          }}
+        </p>
       </div>
 
       <div class="mb-6">
         <label for="password" class="mb-4 font-medium">Password</label>
-        <input 
+        <input
           type="text"
           name="password"
           id="password"
           v-model="loginCredentials.password"
           autocomplete="off"
-          class="block rounded-lg border-2 p-2 shadow-inner shadow-gray-400 w-full transition-colors ease-in-out duration-300
-            hover:border-AccentBlue 
-            focus:outline-none focus:ring-4 focus:ring-AccentBlue "                    
+          class="block rounded-lg border-2 p-2 shadow-inner shadow-gray-400 w-full transition-colors ease-in-out duration-300 hover:border-AccentBlue focus:outline-none focus:ring-4 focus:ring-AccentBlue"
         />
       </div>
 
       <div class="">
         <button
-            type="submit"
-            class="w-full py-2 border border-hidden rounded-lg mb-8 font-semibold bg-AccentBlue text-MainWhite transition ease-in-out duration-300
-            hover:bg-BgBlack/25 hover:text-BgBlack 
-            focus:outline-none focus:ring-4 focus:ring-AccentBlue focus:bg-[#1B8ACC]"
+          type="submit"
+          class="w-full py-2 border border-hidden rounded-lg mb-8 font-semibold bg-AccentBlue text-MainWhite transition ease-in-out duration-300 hover:bg-BgBlack/25 hover:text-BgBlack focus:outline-none focus:ring-4 focus:ring-AccentBlue focus:bg-[#1B8ACC]"
         >
-            Log in
+          Log in
         </button>
       </div>
 
       <div>
-        <RouterLink to="/auth/register" class="font-normal text-sm 
-        focus:outline-AccentBlue ">
-          <p class="p-1">Don't have an account yet? <span class="text-AccentBlue">Sign up</span></p>
+        <RouterLink
+          to="/auth/register"
+          class="font-normal text-sm focus:outline-AccentBlue"
+        >
+          <p class="p-1">
+            Don't have an account yet?
+            <span class="text-AccentBlue">Sign up</span>
+          </p>
         </RouterLink>
       </div>
 
       <div class="inline-flex items-center justify-center w-full">
-        <hr class="w-full h-px translate-y-0.5 my-8 bg-BgBlack border-0">
-        <span class="absolute px-3 font-medium text-BgBlack -translate-x-1/2 bg-MainWhite left-1/2">or</span>
+        <hr class="w-full h-px translate-y-0.5 my-8 bg-BgBlack border-0" />
+        <span
+          class="absolute px-3 font-medium text-BgBlack -translate-x-1/2 bg-MainWhite left-1/2"
+          >or</span
+        >
       </div>
 
-      <RouterLink to="/" class="flex border border-BgBlack justify-center py-3 rounded-lg w-full
-      hover:font-semibold 
-      focus:outline-none focus:ring-4 focus:ring-AccentBlue focus:border-AccentBlue focus:font-medium">
-          <p>Continue without logging in</p>
+      <RouterLink
+        to="/"
+        class="flex border border-BgBlack justify-center py-3 rounded-lg w-full hover:font-semibold focus:outline-none focus:ring-4 focus:ring-AccentBlue focus:border-AccentBlue focus:font-medium"
+      >
+        <p>Continue without logging in</p>
       </RouterLink>
     </div>
   </form>
 </template>
 
 <script setup lang="ts">
-  import { ref } from 'vue'
-  import { AuthErrorCodes, type AuthError } from 'firebase/auth'
+import { ref } from "vue"
+import { AuthErrorCodes, type AuthError } from "firebase/auth"
 
-  import useFirebase from '@/composables/useFirebase'
-  import { useRouter } from 'vue-router'
+import useFirebase from "@/composables/useFirebase"
+import { useRouter } from "vue-router"
 
-  //composables
-  const { login, firebaseUser } = useFirebase()
-  const { replace } = useRouter()
+//composables
+const { login, firebaseUser } = useFirebase()
+const { replace } = useRouter()
 
-  //logic
-  const loginCredentials = ref({
-    email: '',
-    password:'',
-  })
-  const error = ref<AuthError | null>(null)
-  const showError = ref(false)
-  const tooMany = ref(false)
-  const wrongLogin = ref(false)
+//logic
+const loginCredentials = ref({
+  email: "",
+  password: "",
+})
+const error = ref<AuthError | null>(null)
+const showError = ref(false)
+const tooMany = ref(false)
+const wrongLogin = ref(false)
 
-  const isValidEmail = (EmailParameter : string) => {
-    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(EmailParameter) ? true  : false
-  }
+const isValidEmail = (EmailParameter: string) => {
+  return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(EmailParameter)
+    ? true
+    : false
+}
 
-  const handleLogin = () => {
-    tooMany.value = false
-    wrongLogin.value = false
-    if(isValidEmail(loginCredentials.value.email) == true){
-      login(loginCredentials.value.email, loginCredentials.value.password)
+const handleLogin = () => {
+  tooMany.value = false
+  wrongLogin.value = false
+  if (isValidEmail(loginCredentials.value.email) == true) {
+    login(loginCredentials.value.email, loginCredentials.value.password)
       .then(() => {
-        replace('/')
+        replace("/")
       })
       .catch((err: AuthError) => {
         error.value = err
         console.log(error.value.code)
-        if(error.value.code == 'auth/invalid-login-credentials'){
+        if (error.value.code == "auth/invalid-login-credentials") {
           wrongLogin.value = true
         }
-        if(error.value.code == 'auth/too-many-requests'){
+        if (error.value.code == "auth/too-many-requests") {
           tooMany.value = true
         }
       })
-    }
-    else{
-      
-      showError.value = true
-      console.log("email of password is niet goed")
-    }
-
+  } else {
+    showError.value = true
+    console.log("email of password is niet goed")
   }
+}
 </script>
--- packages/pwa/src/views/client/ticket.vue
+++ packages/pwa/src/views/client/ticket.vue
@@ -1,1 +1,54 @@
-<template>Hier komen de tickets</template>
+<template>
+  <RouterLink to="/buyTickets"> Buy tickets </RouterLink>
+
+  <!-- show tickets from user if logged in -->
+  <table class="table-auto w-8/12" v-if="firebaseUser != null">
+    <thead class="">
+      <tr>
+        <th class="pr-8 pl-2 pb-4 border-r">type</th>
+        <th class="pr-8 pb-4 pl-2 border-r">day</th>
+        <th class="pr-8 pb-4 pl-2 border-r">barcode</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="item in ticketResult.getTicketsByUserId"
+        class="odd:bg-black/15"
+      >
+        <div v-if="item.isUsed == false && new Date(item.endDay) > new Date()">
+          <td class="border-r p-2">{{ item.type.name }}</td>
+          <td class="border-r p-2">
+            {{ new Date(item.endDay).toLocaleDateString() }}
+          </td>
+          <td class="border-r p-2">
+            <vue-barcode
+              v-bind:value="item.barcode"
+              v-model="item.barcode"
+              tag="svg"
+            ></vue-barcode>
+          </td>
+        </div>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script setup lang="ts">
+import useFirebase from "@/composables/useFirebase"
+import { GET_ALL_TICKETS_BY_UID } from "@/graphql/allTicketsByUid"
+import VueBarcode from "@chenfengyuan/vue-barcode"
+import { createApp } from "vue"
+import { useQuery } from "@vue/apollo-composable"
+import { computed } from "vue"
+
+const app = createApp({
+  components: {
+    VueBarcode,
+  },
+})
+
+const tickets = useQuery(GET_ALL_TICKETS_BY_UID)
+const ticketResult = computed(() => tickets.result.value)
+
+const { firebaseUser } = useFirebase()
+</script>
